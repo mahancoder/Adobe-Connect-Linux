e94b826db918fdd4caf88d966ff3d36999003091
Mahan Lamee <mahan.lameie87@gmail.com>
2021-11-07 16:57:43 +0330

    Added installer script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+shopt -s extglob
+echo "Copying files..." &&
+INSTALL_PATH="${INSTALL_DIRECTORY:-/usr/bin/adobe/connect}" &&
+mkdir -p $INSTALL_PATH &&
+cp -r !(install.sh) $INSTALL_PATH &&
+echo "Creating URL Scheme handlers..."
+sed -i "s|INSTALL_PATH|$INSTALL_PATH|g" connect.desktop &&
+cp connect.desktop /usr/share/applications &&
+xdg-mime default connect.desktop x-scheme-handler/connectpro &&
+echo "Done!"
