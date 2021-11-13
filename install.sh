3a9ba7aa0334bdf782de26a58ff6264e274bdcaa
Mahan Lamee <mahan.lameie87@gmail.com>
2021-11-13 21:04:06 +0330

    Fixed icon background

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 shopt -s extglob
 echo "Downloading icon..."
 wget https://upload.wikimedia.org/wikipedia/commons/d/d0/Adobe_Connect_icon_%282020%29.svg -O connect.svg
-convert connect.svg connect.png
-rm connect.svg
+convert connect.svg -transparent White connect.png
+rm connect.svg connect-bg.png
 echo "Copying files..." &&
 INSTALL_PATH="${INSTALL_DIRECTORY:-/usr/bin/adobe/connect}" &&
 mkdir -p $INSTALL_PATH &&
