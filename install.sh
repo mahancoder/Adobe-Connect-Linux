7684b3e9914b429bd977bbebf1a6c11d3c61ec2e
Mahan Lamee <mahan.lameie87@gmail.com>
2021-12-05 08:08:38 +0330

    Fixed an issue in the installation

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 shopt -s extglob
 echo "Copying files..." &&
 INSTALL_PATH="${INSTALL_DIRECTORY:-/usr/bin/adobe/connect}" &&
-mkdir -p $INSTALL_PATH &&
+sudo mkdir -p $INSTALL_PATH &&
 sed -i "s|INSTALL_PATH|$INSTALL_PATH|g" open.sh &&
-cp -r !(install.sh) $INSTALL_PATH &&
-chmod +x $INSTALL_PATH/open.sh $INSTALL_PATH/connect
+sudo cp -r !(install.sh) $INSTALL_PATH &&
+sudo chmod +x $INSTALL_PATH/open.sh $INSTALL_PATH/connect
 echo "Creating URL Scheme handlers..."
 sed -i "s|INSTALL_PATH|$INSTALL_PATH|g" connect.desktop &&
-cp connect.desktop /usr/share/applications &&
+sudo cp connect.desktop /usr/share/applications &&
 mkdir -p $HOME/.config &&
 xdg-mime default connect.desktop x-scheme-handler/connectpro &&
 echo "Done!"
