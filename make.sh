2dbb00af64362567f682196fddbb0d605729065a
Mahan Lamee <mahan.lameie87@gmail.com>
2022-07-14 14:01:44 +0430

    Fixed a bug in the make script

--- make.sh
+++ make.sh
@@ -2,13 +2,15 @@
 shopt -s extglob
 echo "Downloading CEF binary..."
 mkdir -p .buildtemp
-wget "https://cef-builds.spotifycdn.com/cef_binary_86.0.24%2Bg85e79d4%2Bchromium-86.0.4240.198_linux64_minimal.tar.bz2" -O .buildtemp/cef.tar.bz2
+wget "https://cef-builds.spotifycdn.com/cef_binary_86.0.24%2Bg85e79d4%2Bchromium-86.0.4240.198_linux64_minimal.tar.bz2" -O .buildtemp/cef.tar.bz2 &&
 echo "Extracting..."
-tar -xjf .buildtemp/cef.tar.bz2 --strip-components=1 -C .buildtemp/
+tar -xjf .buildtemp/cef.tar.bz2 --strip-components=1 -C .buildtemp/ &&
 cd .buildtemp
 echo "Building libcef_dll_wrapper"
-cmake .
-make libcef_dll_wrapper -j$(($(grep -c ^processor /proc/cpuinfo) * 2))
+# Fix build bug in CEF binary
+sed -i '/-Werror/d' ./cmake/cef_variables.cmake
+cmake . &&
+make libcef_dll_wrapper -j`nproc` &&
 cd ..
 mkdir -p Release
 echo "Copying libcef_dll_wrapper.a..."
@@ -20,7 +22,7 @@ cp -a .buildtemp/Resources/. Release/
 echo "Stripping libcef.so to reduce it's size..."
 strip Release/libcef.so
 echo "Compiling the app..."
-g++ main.cpp MyApp.cpp -I. -L./Release -lcef -lpthread -lX11 -lcef_dll_wrapper -Wl,-rpath,. -o connect
+g++ main.cpp MyApp.cpp -I. -L./Release -lcef -lpthread -lX11 -lcef_dll_wrapper -Wl,-rpath,. -o connect &&
 echo "Copying app files..."
 cp -a {install.sh,connect.desktop,open.sh,connect,LICENSE,connect.png} Release/
 rm -r .buildtemp/*
